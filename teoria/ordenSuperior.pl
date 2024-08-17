20611b5133d547f2900123ac38b00adf89213ea6
Gianlucca Bolocco <gbolocco@frba.utn.edu.ar>
2024-08-17 03:21:38 +0000

    video 2

--- teoria/ordenSuperior.pl
+++ teoria/ordenSuperior.pl
@@ -42,3 +42,71 @@ Animal = tigre ;
 false.
 
 */
+% =====
+
+habitat(foca,costa).
+habitat(foca,tundra).
+% templado(Bioma).
+templado(costa).
+
+% friolento(Animal).
+friolento(Animal):-
+    habitat(Animal,Bioma),
+    templado(Bioma).
+
+/*
+1 ?- habitat(foca,Bioma).
+Bioma = costa ;
+Bioma = tundra.
+
+2 ?- friolento(foca).
+true --> RESPUESTA INCORRECTA, YA QUE LA TUNDRA NO ES TEMPLADA
+*/
+
+% === Predicado forall/2 ===
+friolento2(Animal):-
+    animal(Animal), % con esta linea ligo la variable
+    forall(habitat(Animal,Bioma),templado(Bioma)).
+/*
+1 ?- friolento2(foca).     
+false. --> AHORA SI.
+*/
+
+% === PRACTICA ===
+
+% come(Comedor,Comido).
+come(Comedor,Comido).
+
+% 1) hostil
+hostil(Animal,Bioma):-
+    animal(Animal),
+    habitat(_,Bioma),
+    forall(habitat(OtroAnimal,Bioma),come(OtroAnimal,Animal)).
+
+% 2) terrible
+terrible(Animal,Bioma):-
+    animal(Animal),
+    habitat(_,Bioma),
+    forall(come(OreoAnimal,Animal),habitat(OtroAnimal,Bioma)).
+
+% 3) compatibles
+compatibles(UnAnimal,OtroAnimal):-
+    animal(UnAnimal),
+    animal(OtroAniaml),
+    not(come(UnAnimal,OtroAnimal)),
+    not(come(OtroAnimal,UnAnimal)).
+
+% 4) adaptable
+adaptable(Animal):-
+    animal(Animal),
+    forall(habitat(_,Bioma),habitat(Animal,Bioma)).
+
+% 5) raro
+raro(Animal):-
+    habitat(Aniaml,Bioma),
+    not((habitat(Animal,OtroBioma), Bioma \= OtroBioma)).
+
+% 6)dominante
+dominante(Animal):-
+    habitat(Animal,Bioma),
+    forall(habitat(OtroAnimal,Bioma),come(Animal,OtroAnimal)).
